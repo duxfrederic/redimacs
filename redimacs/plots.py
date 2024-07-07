4db2b4e0b19faa14092f90620056f49389642d5f
Frédéric Dux <duxfrederic@gmail.com>
2024-07-06 20:03:51 -0400

    higher dpi plot

--- redimacs/plots.py
+++ redimacs/plots.py
@@ -3,10 +3,10 @@ from astropy.visualization import simple_norm
 
 
 def show_image(array, output_path):
-    plt.figure()
+    plt.figure(figsize=(15, 4))
     norm = simple_norm(array, stretch='asinh', percent=99.9, asinh_a=0.05)
     plt.imshow(array, origin='lower', norm=norm)
     plt.axis('off')
     plt.tight_layout()
-    plt.savefig(output_path, format='jpg', dpi=400)
+    plt.savefig(output_path, format='jpg', dpi=500)
     plt.close()
